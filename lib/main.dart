377ca3775838ddba844f43d2fe364abd21224362
whatever125 <kolmakov125@gmail.com>
2021-04-28 22:29:12 +0700

    Изменен макет приложения, добавлено нижнее меню

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart' show timeDilation;
 import 'dart:math';
 
 
 void main() {
+  timeDilation = 1.5;
   runApp(MyApp());
 }
 
@@ -12,16 +14,96 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'PROFIВЫБОР',
+      home: TabsPage(),
       theme: ThemeData(
+        brightness: Brightness.light,
         primaryColor: Colors.white,
+        textTheme: TextTheme(
+          headline6: TextStyle(fontSize: 36.0),
+        ),
       ),
-      home: Feed(),
-      routes: <String, WidgetBuilder> {
-        Search.routeName : (BuildContext context) => Search(),
-        Favorite.routeName : (BuildContext context) => Favorite()
-      }
+      darkTheme: ThemeData(
+        brightness: Brightness.dark,
+      ),
+      themeMode: ThemeMode.system,
     );
   }
+}
+
+
+class TabsPage extends StatefulWidget {
+  @override
+  _TabsPageState createState() => _TabsPageState();
+}
+
+class _TabsPageState extends State<TabsPage> {
+  int _currentIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(TabNavigationItem.items[_currentIndex].label),
+      ),
+      drawer: NavDrawer(),
+      body: IndexedStack(
+        index: _currentIndex,
+        children: [
+          for (final tabItem in TabNavigationItem.items) tabItem.page,
+        ],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentIndex,
+        onTap: (int index) => setState(() => _currentIndex = index),
+        items: [
+          for (final tabItem in TabNavigationItem.items)
+            BottomNavigationBarItem(
+              icon: tabItem.icon,
+              activeIcon: tabItem.activeIcon,
+              label: tabItem.label,
+            )
+        ],
+        selectedItemColor: Colors.blue,
+        unselectedItemColor: Colors.grey,
+      ),
+    );
+  }
+}
+
+
+class TabNavigationItem {
+  final Widget page;
+  final String label;
+  final Icon activeIcon;
+  final Icon icon;
+
+  TabNavigationItem({
+    @required this.page,
+    @required this.label,
+    @required this.activeIcon,
+    @required this.icon,
+  });
+
+  static List<TabNavigationItem> get items => [
+    TabNavigationItem(
+      page: Feed(),
+      label: "Лента",
+      activeIcon: Icon(Icons.home),
+      icon: Icon(Icons.home_outlined),
+    ),
+    TabNavigationItem(
+      page: Search(),
+      label: "Поиск",
+      activeIcon: Icon(Icons.search),
+      icon: Icon(Icons.search_outlined),
+    ),
+    TabNavigationItem(
+      page: Profile(),
+      label: "Профиль",
+      activeIcon: Icon(Icons.person),
+      icon: Icon(Icons.person_outlined),
+    ),
+  ];
 }
 
 
@@ -38,10 +120,6 @@ class FeedState extends State<Feed> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Лента'),
-      ),
-      drawer: NavDrawer(),
       body: _buildFeed(),
     );
   }
@@ -78,16 +156,12 @@ class Search extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      drawer: NavDrawer(),
-      appBar: AppBar(
-        title: Text('Поиск работы'),
-      ),
       body: Center(
         child: Text(
-            'Search',
-            style: TextStyle(
-                fontSize: 24.0
-            )
+          'Search',
+          style: TextStyle(
+            fontSize: 24.0
+          )
         ),
       ),
     );
@@ -101,16 +175,12 @@ class Favorite extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      drawer: NavDrawer(),
-      appBar: AppBar(
-        title: Text('Избранное'),
-      ),
       body: Center(
         child: Text(
-            'Favorites',
-            style: TextStyle(
-                fontSize: 24.0
-            )
+          'Favorites',
+          style: TextStyle(
+            fontSize: 24.0
+          )
         ),
       ),
     );
@@ -124,16 +194,12 @@ class Profile extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      drawer: NavDrawer(),
-      appBar: AppBar(
-        title: Text('Профиль'),
-      ),
       body: Center(
         child: Text(
-            'Profile',
-            style: TextStyle(
-                fontSize: 24.0
-            )
+          'Profile',
+          style: TextStyle(
+            fontSize: 24.0,
+          )
         ),
       ),
     );
@@ -154,67 +220,45 @@ class NavDrawer extends StatelessWidget {
                   accountName: Text('Иванов Иван'),
                   accountEmail: Text('ivanov@ivan.org'),
                   currentAccountPicture: CircleAvatar(
-                      backgroundColor: Colors.blue,
-                      child: Text('ИИ')
+                    foregroundColor: Colors.white,
+                    backgroundColor: Colors.blue,
+                    child: Text('ИИ')
                   ),
-                ),
-                ListTile(
-                  title: Text('Лента'),
-                  leading: Icon(Icons.today),
-                  onTap: () => {
-                    Navigator.of(context).push(
-                        PageRouteBuilder(
-                            pageBuilder: (BuildContext context, _, __) => Feed()
-                        )
-                    )
-                  },
-                ),
-                ListTile(
-                  title: Text('Поиск работы'),
-                  leading: Icon(Icons.search),
-                  onTap: () => {
-                    Navigator.of(context).push(
-                        PageRouteBuilder(
-                            pageBuilder: (BuildContext context, _, __) => Search()
-                        )
-                    )
-                  },
                 ),
                 ListTile(
                   title: Text('Избранное'),
                   leading: Icon(Icons.star_outline),
-                  onTap: () => {
-                    Navigator.of(context).push(
-                        PageRouteBuilder(
-                            pageBuilder: (BuildContext context, _, __) => Favorite()
-                        )
-                    )
-                  },
+                  onTap: () => {Navigator.of(context).pop()},
+                ),
+                ListTile(
+                  title: Text('Настройки'),
+                  leading: Icon(Icons.settings_outlined),
+                  onTap: () => {Navigator.of(context).pop()},
+                ),
+                ListTile(
+                  title: Text('Помощь'),
+                  leading: Icon(Icons.help_outline),
+                  onTap: () => {Navigator.of(context).pop()},
                 ),
               ],
             ),
           ),
           Container(
-              child: Align(
-                  alignment: FractionalOffset.bottomCenter,
-                  child: Container(
-                      child: Column(
-                        children: <Widget>[
-                          Divider(),
-                          ListTile(
-                            title: Text('Помощь'),
-                            leading: Icon(Icons.help_outline),
-                            onTap: () => {Navigator.of(context).pop()},
-                          ),
-                          ListTile(
-                            title: Text('Выйти'),
-                            leading: Icon(Icons.logout),
-                            onTap: () => {Navigator.of(context).pop()},
-                          ),
-                        ],
-                      )
-                  )
+            child: Align(
+              alignment: FractionalOffset.bottomCenter,
+              child: Container(
+                child: Column(
+                  children: <Widget>[
+                    Divider(),
+                    ListTile(
+                      title: Text('Выйти'),
+                      leading: Icon(Icons.logout),
+                      onTap: () => {Navigator.of(context).pop()},
+                    ),
+                  ],
+                )
               )
+            )
           )
         ],
       ),
